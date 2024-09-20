fa03d48742d687a0ad515dea8fbdd18ad0999007
panz803 <s1411031011@nutc.edu.tw>
2024-09-21 02:02:59 +0800

    把導覽列變成父模板

--- TRYCO2/CO2_final/CO2/cm_employee.php
+++ TRYCO2/CO2_final/CO2/cm_employee.php
@@ -19,80 +19,8 @@ if (!isset($_SESSION['em_id'])) {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
-        <!-- 導覽列 -->
-        <nav class="navbar navbar-expand-lg navbar-custom">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="cm_index.php"><img class="logo" src="img/logo.png" alt=""></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">管理者首頁</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">員工資料</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                交通車資料
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-custom">
-                                <li><a class="dropdown-item" href="#">管理交通車</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                碳排紀錄
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-custom">
-                                <li><a class="dropdown-item" href="#">交通車碳排紀錄</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">員工碳排紀錄</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">回報問題</a>
-                        </li>
-
-                        <?php
-                        if (isset($_SESSION['em_name'])) {
-                            $user_name = $_SESSION['em_name'];
-                        ?>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <?php echo $user_name; ?> <!-- 顯示用戶名 -->
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-custom">
-                                <li><a class="dropdown-item" href="em_index.php">員工首頁</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="cm_index.php">管理者首頁</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li>
-                                    <form method="post" class="dropdown-item"> <!-- 登出按鈕放在表單裡 -->
-                                        <input type="submit" name="logout" value="登出" class="btn btn-success"> <!-- 使用btn-link樣式讓它看起來像連結 -->
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-
-                        <?php
-                        } else {
-                            // 若未登入顯示的項目
-                            echo "<li><a class='nav-link'>未登入</a></li>";
-                        }
-
-                        // 登出功能
-                        if (isset($_POST["logout"])) {
-                            include_once('inc/log_out.inc');
-                        }
-                        ?>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+        <!-- 導入導覽列 -->
+        <?php include('nav.php') ?>
 
         <div class="container">
             <!-- 表格 -->
