6f7ff2ae6703f373dfdefc9636751547daabe3cc
panz803 <s1411031011@nutc.edu.tw>
2024-09-22 18:31:34 +0800

    交通車資料畫面完成

--- TRYCO2/CO2_final/CO2/cm_car.php
+++ TRYCO2/CO2_final/CO2/cm_car.php
@@ -34,6 +34,9 @@ $offset = ($pages - 1) * $records_per_page;
         <title>交通車資料</title>
         <link rel="stylesheet" href="css/cm_car.css" type="text/css">
         <link rel="shortcut icon" href="img/logo.png" >
+        <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+        <script src="https://npmcdn.com/flatpickr@4.6.3/dist/l10n/zh-tw.js"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     </head>
     <body>
@@ -45,18 +48,18 @@ $offset = ($pages - 1) * $records_per_page;
 
         <div class="container">
             <!-- 篩選列 -->
-            <form action="" method="post" class="row g-3 d-flex align-items-center">
+            <form action="" method="post" class="row g-3 d-flex align-items-center filter-form">
                 <!-- 篩選日期 -->
-                <div class="col-md-3 d-flex justify-content-center align-items-center">
-                    <label for="date">篩選日期：</label>
-                    <input type="date" id="date" name="date">
+                <div class="col-12 col-lg-6 col-xl-4 d-flex align-items-center">
+                    <label for="date_range">篩選日期：</label>
+                    <input type="text" id="date_range" name="date_range" class="date-range-picker" placeholder="選擇日期">
                 </div>
 
                 <!-- 篩選交通車 -->
-                <div class="col-md-3 d-flex justify-content-center align-items-center">
-                    <label for="car">篩選交通車：</label>
-                    <select>
-                        <option selected>請選擇</option>
+                <div class="col-12 col-lg-6 col-xl-3 d-flex justify-content-center align-items-center">
+                    <label for="filter_car">篩選交通車：</label>
+                    <select id="filter_car" name="filter_car">
+                        <option value="">請選擇</option>
                         <?php
                         include_once("dbcontroller.php");
                         $db_handle = new DBController();
@@ -76,10 +79,10 @@ $offset = ($pages - 1) * $records_per_page;
                 </div>
 
                 <!-- 篩選員工 -->
-                <div class="col-md-3 d-flex justify-content-center align-items-center">
-                    <label for="employee">篩選員工：</label>
-                    <select>
-                        <option selected>請選擇</option>
+                <div class="col-12 col-lg-6 col-xl-3 d-flex justify-content-center align-items-center">
+                    <label for="filter_employee">篩選員工：</label>
+                    <select id="filter_employee" name="filter_employee">
+                        <option value="">請選擇</option>
                         <?php
                         $sql = "SELECT em_id, em_name FROM employee";
                         $result = $db_handle->runQuery($sql);
@@ -96,8 +99,9 @@ $offset = ($pages - 1) * $records_per_page;
                 </div>
 
                 <!-- 確認篩選按鈕 --> 
-                <div class="col-md-3 d-flex justify-content-center align-items-end">
-                    <button type="button" class="btn btn-success btn-lg">確認篩選</button>
+                <div class="col-12 col-lg-6 col-xl-2 d-flex justify-content-center align-items-end">
+                    <!-- type="submit"設定提交表單 -->
+                    <button type="submit" class="btn btn-success btn-lg" name="apply_filter" data-style="apply_filter">確認篩選</button>
                 </div>
             </form>
 
@@ -194,6 +198,50 @@ $offset = ($pages - 1) * $records_per_page;
                 </table>
             </div>
 
+            <!-- 切換頁面按鈕 -->
+            <?php
+            echo "<div class='turn_pages_div'>";
+
+            // 顯示最前面一頁和上一頁
+            if ($pages > 1) {
+                echo "<a class='turn_pages' href='cm_car.php?pages=1'><<</a> ";
+                echo "<a class='turn_pages' href='cm_car.php?pages=" . ($pages - 1) . "'><</a> ";
+            } else {
+                echo "<a class='Noturn_pages'><<</a> ";
+                echo "<a class='Noturn_pages'><</a> ";
+            }
+
+            // 確定分頁範圍
+            $start_page = max(1, $pages - 4);
+            $end_page = min($total_pages, $start_page + 8);
+
+            if ($start_page > 1) {
+                echo "<a class='turn_pages_more'> ...<a>";
+            }
+
+            for ($i = $start_page; $i <= $end_page; $i++) {
+                if ($i != $pages) {
+                    echo "<a class='turn_pages' href='cm_car.php?pages=" . $i . "' onclick='fetchData()'>" . $i . " </a>";
+                } else {
+                    echo "<a class='Noturn_pages'>" . $i . " </a>";
+                }
+            }
+
+            if ($end_page < $total_pages) {
+                echo "<a class='turn_pages_more'> ...<a>";
+            }
+
+            // 顯示下一頁和最後面一頁
+            if ($pages < $total_pages) {
+                echo "<a class='turn_pages' href='cm_car.php?pages=" . ($pages + 1) . "'>></a> ";
+                echo "<a class='turn_pages' href='cm_car.php?pages=" . $total_pages . "'>>></a>";
+            } else {
+                echo "<a class='Noturn_pages'>></a> ";
+                echo "<a class='Noturn_pages'>>></a>";
+            }
+
+            echo "</div>";
+            ?>
         </div>
 
         <script>
@@ -217,8 +265,56 @@ $offset = ($pages - 1) * $records_per_page;
                 document.body.scrollTop = 0; // Safari
                 document.documentElement.scrollTop = 0; // Chrome、Firefox、 IE、Opera
             }
-        </script>
 
+            // 日期選擇器(區間)
+            flatpickr("#date_range", {
+                mode: "range",
+                dateFormat: "Y-m-d",
+                "locale": "zh_tw", 
+                onReady: function(selectedDates, dateStr, instance) {
+                    const container = instance.calendarContainer;
+                    const weekButton = document.createElement("button");
+                    weekButton.textContent = "本周";
+                    weekButton.type = "button";
+                    weekButton.classList.add("flatpickr_week_button");
+                    weekButton.addEventListener("click", function() {
+                        instance.setDate(getWeekRange());
+                    });
+
+                    const monthButton = document.createElement("button");
+                    monthButton.textContent = "本月";
+                    monthButton.type = "button";
+                    monthButton.classList.add("flatpickr_week_button");
+                    monthButton.addEventListener("click", function() {
+                        instance.setDate(getMonthRange());
+                    });
+
+                    container.appendChild(weekButton);
+                    container.appendChild(monthButton);
+                }
+            });
+
+            function getWeekRange() {
+                const now = new Date();
+                const dayOfWeek = now.getDay();
+                const start = new Date(now);
+                const end = new Date(now);
+
+                start.setDate(now.getDate() - dayOfWeek + 1); // 設定到本周的星期一
+                end.setDate(now.getDate() + (7 - dayOfWeek)); // 設定到本周的星期日
+
+                return [start, end];
+            }
+
+            function getMonthRange() {
+                const now = new Date();
+                const start = new Date(now.getFullYear(), now.getMonth(), 1); // 設定到本月的第一天
+                const end = new Date(now.getFullYear(), now.getMonth() + 1, 0); // 設定到本月的最後一天
+
+                return [start, end];
+            }
+            
+        </script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
