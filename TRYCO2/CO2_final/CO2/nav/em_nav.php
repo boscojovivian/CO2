8511c5398bd354a07c0720658e50298850dc1401
JoJo <jojojojoyang@gmail.com>
2024-11-12 22:12:16 +0800

    員工的導覽列logo鏈結錯誤修正

--- TRYCO2/CO2_final/CO2/nav/em_nav.php
+++ TRYCO2/CO2_final/CO2/nav/em_nav.php
@@ -44,7 +44,7 @@
 <!-- 導覽列 -->
         <nav class="navbar navbar-expand-lg navbar-custom">
             <div class="container-fluid">
-                <a class="navbar-brand" href="cm_index.php"><img class="logo" src="img/logo.png" alt=""></a>
+                <a class="navbar-brand" href="em_index.php"><img class="logo" src="img/logo.png" alt=""></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
